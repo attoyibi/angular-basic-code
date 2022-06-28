995c57372c0941a5ef8b96c10b9595ba9ecb5925
alterra-muchson <muchson@alterra.id>
2022-06-28 22:11:47 +0700

    gitignore angular n node module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { XyzComponent } from './xyz/xyz.component';
-import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    XyzComponent,
-    HeaderComponent
+
   ],
   imports: [
     BrowserModule
