b42a2da0ba26526adcaa0dec378c807db2f20eb7
attoyibi <muchson@alterra.id>
2022-10-10 22:32:13 +0700

    proxy setting

--- recipes-book-api/server.js
+++ recipes-book-api/server.js
@@ -1,37 +1,42 @@
-const fs = require('fs')
-const jsonServer = require('json-server')
-const server = jsonServer.create()
+const fs = require("fs");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
 server.use(jsonServer.defaults());
-const recipes = JSON.parse(fs.readFileSync('./db-json/recipes.json', 'UTF-8'));
+const recipes = JSON.parse(fs.readFileSync("./db-json/recipes.json", "UTF-8"));
 
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 8081 });
 let timerId = null;
 
-server.get('/api/recipes', (req, res) => {
-  res.status(200).json(recipes)
-})
+server.get("/api/recipes", (req, res) => {
+  console.log("masuk /api/recipes");
+  res.status(200).json(recipes);
+});
 
-server.post('/api/recipes/save', (req, res) => {
-  res.status(200).json(req.body)
-})
+server.get("/db", (req, res) => {
+  console.log("masuk /db");
+  res.status(200).json(recipes);
+});
+
+server.post("/api/recipes/save", (req, res) => {
+  res.status(200).json(req.body);
+});
 
 server.listen(3001, () => {
-  console.log('Run Auth API Server')
-})
+  console.log("Run Auth API Server");
+});
 
-wss.on('connection', ws => {
+wss.on("connection", (ws) => {
   onConnection(ws);
-  ws.on('message', message => {
+  ws.on("message", (message) => {
     onMessage(message, ws);
   });
-  ws.on('error', error => {
+  ws.on("error", (error) => {
     OnError(error);
   });
-  ws.on('close', ws => {
+  ws.on("close", (ws) => {
     onClose();
-  })
-
+  });
 });
 
 function onConnection(ws) {
@@ -45,7 +50,7 @@ function onConnection(ws) {
 
 function onClose() {
   timerId = null;
-  console.log('connection closed');
+  console.log("connection closed");
 }
 
 function OnError(error) {
